30c5db5d83ff77a11d37bccaa0737b8058ec6912
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-01-22 21:51:31 +0200

    feat: added the solution of lab8 part1

---
+++ lab8/part1/main.js
@@ -0,0 +1,50 @@
+let active = 0;
+
+let sliderItems = document.querySelectorAll("section .item");
+
+let showSlider = () => {
+
+    if (active > 5) {
+        active = 0;
+    }
+    
+    if (active < 0) {
+        active = 5;
+    }
+
+    sliderItems.forEach(item => {
+        item.cssText = `
+            transform: none;
+            opacity: 0;
+        `;
+        item.classList.remove("active");
+        item.classList.remove("prev");
+        item.classList.remove("next");
+    })
+
+    sliderItems[active].classList.add("active");
+
+    let prevIndex = (active - 1 + sliderItems.length) % sliderItems.length,
+        nextIndex = (active + 1) % sliderItems.length;
+
+    sliderItems[prevIndex].classList.add("prev");
+    sliderItems[nextIndex].classList.add("next");
+
+}
+
+showSlider(active);
+
+
+let nextBtn = document.querySelector(".nextBtn"),
+    prevBtn = document.querySelector(".prevBtn");
+
+nextBtn.addEventListener('click', () => {
+
+    showSlider(++active);
+
+})
+
+
+prevBtn.addEventListener('click', () => {
+    showSlider(--active);
+})
